db3c23e3e39744dbd4c55bbe21dffff4a253fc76
root <root@derekstevens.net>
2024-11-07 16:36:32 -0700

    Added *

--- doit.sh
+++ doit.sh
@@ -1,6 +1,6 @@
 git commit
 git push
-rm -rf /var/www/derekstevens
+rm -rf /var/www/derekstevens/*
 rm -rf /home/derek/programming/derekstevens/public
 hugo
 cp -rf /home/derek/programming/derekstevens/public/* /var/www/derekstevens/
